2ff70ac331f8dbe0bc5bc4e32ade1dae35a098f5
Niki Mirabet <79007735+nmirabets@users.noreply.github.com>
2025-02-17 09:13:24 +0100

    added vector-db notebook

--- agent/prompts.py
+++ agent/prompts.py
@@ -1,11 +1,11 @@
 from agent.sakila_schema import SAKILA_SCHEMA
 
-SYSTEM_PROMPT = """
+SYSTEM_PROMPT = f"""
 - You are a helpful assistant and an expert data analyst that can answer questions about the sakila database. 
 - Use the get_data_df tool to get the data from the database. 
 - Generate SQL queries following the schema of the database. 
 
-DATABSE SCHEMA:
+DATABASE SCHEMA:
 
 {SAKILA_SCHEMA}
 
--- app.py
+++ app.py
@@ -1,3 +1,46 @@
+########################################################
+# Import libraries
+########################################################
 import streamlit as st
+from agent.agent import agent
+from agent.prompts import SYSTEM_PROMPT
 
-st.title("🤖Sakila Data Agent")
+########################################################
+# Streamlit app Title
+########################################################
+st.title("💬 Sakila Data Agent")
+
+########################################################
+# Initialize session state
+########################################################
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "system", 
+         "content": SYSTEM_PROMPT},
+        {"role": "assistant", 
+         "content": "How can I help you?"}
+    ]
+
+########################################################
+# Display messages
+########################################################
+for msg in st.session_state.messages[1:]:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+########################################################
+# Get user input
+########################################################
+if prompt := st.chat_input():
+
+    # Add user message to session state
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message
+    st.chat_message("user").write(prompt)
+    
+    # Get agent response
+    msg = agent(st.session_state.messages)
+
+    # Add agent response to session state
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    # Display agent response
+    st.chat_message("assistant").write(msg)
